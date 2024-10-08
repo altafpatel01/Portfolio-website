5f25d226738f4a510fd1e55e87686fd4bea0e237
Altaf <altafpatel8485@gmail.com>
2024-10-08 22:37:50 +0530

    layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { HiOutlineMail } from 'react-icons/hi'
 
 const Footer =()=> {
   return (
-    <div className="flex md:hidden bg-[#0a192f] justify-center">
+    <div className="flex md:hidden bg-[#0a192f] pb-4 justify-center">
         <ul className='flex'>
           <li className="w-[60px] hover:w-[64px] h-[60px] flex justify-center items-center   bg-blue-600">
             <a
@@ -39,30 +39,7 @@ const Footer =()=> {
                <BsFillPersonLinesFill className='fill-white' size={30} />
             </a>
           </li>
-          {/* <li className="w-[60px] hover:w-[64px] h-[60px] justify-center items-center bg-[#333333]">
-            <a
-              className=" w-full flex justify-center items-center text-gray-300"
-              href="https://github.com/altafpatel01"
-            >
-            <FaGithub size={30} />
-            </a>
-          </li>
-          <li className="w-[60px] hover:w-[64px] h-[60px] justify-center items-center  bg-[#6fc2b0]">
-            <a
-              className=" w-full flex justify-center items-center text-gray-300"
-              href="mailto:altafpatel8485@gmail.com"
-            >
-             <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className="w-[60px] hover:w-[64px] h-[60px] justify-center items-center  bg-[#565f69]">
-            <a
-              className=" w-full flex justify-center items-center text-gray-300"
-              href="/"
-            >
-              <BsFillPersonLinesFill size={30} />
-            </a>
-          </li> */}
+
         </ul>
       </div>
     
